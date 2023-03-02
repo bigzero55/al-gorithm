44fcf4ba04ee65cda05b90897700c7c4ed99a8ac
Abdul Rojak <abdulrozzaaq5@outlook.co.id>
2023-03-02 14:44:08 +0700

    nomor 3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// #1
+// #1 =====================================================
 let _string =  "NEGIE1"
 
 function ReverseWithoutNumber (str) {
@@ -10,17 +10,49 @@ function ReverseWithoutNumber (str) {
    return joinArray + cutNumber;
 }
 
-// #2
+console.log(ReverseWithoutNumber(_string))
+
+// #2 =====================================================
 
 const sentence = "Saya sangat senang mengerjakan soal algoritma"
 
-function getLogest (str) {
+function getLongest (str) {
    let arrayNumber = []
    const _array =  str.split(" ")
     _array.map(elm => {
       arrayNumber.push(elm.length)
    })
-   return Math.max(...arrayNumber)
+return `${Math.max(...arrayNumber)} character`
 }
 
-console.log(getLogest(sentence))
+console.log(getLongest(sentence))
+
+// #3 =====================================================
+
+const INPUT = ['xc', 'dz', 'bbb', 'dz']  
+const QUERY = ['bbb', 'ac', 'dz']  
+
+
+function getQuery(query){
+   const getQueryInINPUT = INPUT.filter(elm => elm === query)
+   if (getQueryInINPUT.length != 0) {
+      return `"${query}" ada ${getQueryInINPUT.length}`
+   } else {
+      return  `"${query}" tidak ada`
+   }
+}
+
+function getResult(query) {
+   const getQueryInINPUT = INPUT.filter(elm => elm === query)
+   return getQueryInINPUT.length
+}
+
+function conclusion(query) {
+   const _array =  query.map( elm => getResult(elm))
+   const conclus =  query.map( elm => getQuery(elm))
+   return `OUTPUT [${_array}] = ${conclus.join(' ')}`
+}
+
+console.log(conclusion(QUERY))
+
+// #4 =====================================================
